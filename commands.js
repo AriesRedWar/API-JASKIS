a305b7e724aea2f983977268ee9c5c157070d3d9
AriesRedWar <philipjbowman5@gmail.com>
2022-09-01 21:22:49 -0400

    all ciommands added

--- commands.js
+++ commands.js
@@ -3,35 +3,247 @@
 
 // GETTING STARTED
 // 1. Create a database called jaskis
-
+use jaskis
 
 // 2. Create a collection called bounties
-
+db.createCollection('bounties')
 
 // ADD THE ANIMAL BOUNTIES
 // 1. Insert the given "Thanoceros" bounty object
-
+db.bounties.insertOne({
+    name: "Thanoceros",
+    species: "Rhinoceros",
+    location: "Grasslands",
+    wantedFor: "Eating too much grass",
+    client: "Songbird",
+    reward: 10000,
+    captured: false
+  })
+  { acknowledged: true,
+    insertedId: ObjectId("631133d709bab9eaf715c2a3") }
 
 // 2. Query for all bounties in the bounties collection
-
+db.bounties.find()
+{ _id: ObjectId("631133d709bab9eaf715c2a3"),
+  name: 'Thanoceros',
+  species: 'Rhinoceros',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  client: 'Songbird',
+  reward: 10000,
+  captured: false }
 
 // 3. Insert many bounties at once using the given objects
+b.bounties.insertMany([
+    {
+      "name": "Nebullama",
+      "species": "Llama",
+      "location": "Grasslands",
+      "wantedFor": "Drinking all the water from the ocean",
+      "client": "Songbird",
+      "reward": 2500,
+      "captured": false
+    },
+    {
+      "name": "Polarwind",
+      "species": "Polar Bear",
+      "location": "Arctic",
+      "wantedFor": "Not hibernating",
+      "client": "Sabertooth",
+      "reward": 4000,
+      "captured": false
+    },
+    {
+      "name": "Wrecking Crows",
+      "species": "Crow",
+      "location": "Grasslands",
+      "wantedFor": "Cawing too loudly",
+      "client": "Red wolf",
+      "reward": 40000,
+      "captured": false
+    },
+    {
+      "name": "Grandhog",
+      "species": "Groundhog",
+      "location": "Woodlands",
+      "wantedFor": "Not coming out of the hole on time and prolonging winter",
+      "client": "Songbird",
+      "reward": 50000,
+      "captured": false
+    },
+    {
+      "name": "Grim Panda",
+      "species": "Giant Panda",
+      "location": "Temperate forest",
+      "wantedFor": "Eating all the bamboo",
+      "client": "Red wolf",
+      "reward": 5000,
+      "captured": false
+    }
+  ])
+  { acknowledged: true,
+    insertedIds: 
+     { '0': ObjectId("6311376909bab9eaf715c2a6"),
+       '1': ObjectId("6311376909bab9eaf715c2a7"),
+       '2': ObjectId("6311376909bab9eaf715c2a8"),
+       '3': ObjectId("6311376909bab9eaf715c2a9"),
+       '4': ObjectId("6311376909bab9eaf715c2aa") } }
 
 // MANAGE THE DATABASE
 // Queries
 // 1. Query for all bounties in the Grasslands
-
+db.bounties.find({location:'Grasslands'})
+{ _id: ObjectId("631133d709bab9eaf715c2a3"),
+  name: 'Thanoceros',
+  species: 'Rhinoceros',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  client: 'Songbird',
+  reward: 10000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a6"),
+  name: 'Nebullama',
+  species: 'Llama',
+  location: 'Grasslands',
+  wantedFor: 'Drinking all the water from the ocean',
+  client: 'Songbird',
+  reward: 2500,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a8"),
+  name: 'Wrecking Crows',
+  species: 'Crow',
+  location: 'Grasslands',
+  wantedFor: 'Cawing too loudly',
+  client: 'Red wolf',
+  reward: 40000,
+  captured: false }
 // 2. Query for all bounties with a reward worth 10000 or more
-
+db.bounties.find({reward:{ $gte:10000}})
+{ _id: ObjectId("631133d709bab9eaf715c2a3"),
+  name: 'Thanoceros',
+  species: 'Rhinoceros',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  client: 'Songbird',
+  reward: 10000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a8"),
+  name: 'Wrecking Crows',
+  species: 'Crow',
+  location: 'Grasslands',
+  wantedFor: 'Cawing too loudly',
+  client: 'Red wolf',
+  reward: 40000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a9"),
+  name: 'Grandhog',
+  species: 'Groundhog',
+  location: 'Woodlands',
+  wantedFor: 'Not coming out of the hole on time and prolonging winter',
+  client: 'Songbird',
+  reward: 50000,
+  captured: false }
 // 3. Query for all bounties, but exclude the client attribute from being shown
-
+db.bounties.find({reward: {$gte:0}},{client: 0})
+{ _id: ObjectId("631133d709bab9eaf715c2a3"),
+  name: 'Thanoceros',
+  species: 'Rhinoceros',
+  location: 'Grasslands',
+  wantedFor: 'Eating too much grass',
+  reward: 10000,
+  captured: false }
+{ _id: ObjectId("631136f509bab9eaf715c2a5"),
+  name: 'Lokinkajou',
+  species: 'Kinkajou',
+  location: 'Tropical rainforest',
+  wantedFor: 'Partying too late at night',
+  reward: 1000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a6"),
+  name: 'Nebullama',
+  species: 'Llama',
+  location: 'Grasslands',
+  wantedFor: 'Drinking all the water from the ocean',
+  reward: 2500,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a7"),
+  name: 'Polarwind',
+  species: 'Polar Bear',
+  location: 'Arctic',
+  wantedFor: 'Not hibernating',
+  reward: 4000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a8"),
+  name: 'Wrecking Crows',
+  species: 'Crow',
+  location: 'Grasslands',
+  wantedFor: 'Cawing too loudly',
+  reward: 40000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2a9"),
+  name: 'Grandhog',
+  species: 'Groundhog',
+  location: 'Woodlands',
+  wantedFor: 'Not coming out of the hole on time and prolonging winter',
+  reward: 50000,
+  captured: false }
+{ _id: ObjectId("6311376909bab9eaf715c2aa"),
+  name: 'Grim Panda',
+  species: 'Giant Panda',
+  location: 'Temperate forest',
+  wantedFor: 'Eating all the bamboo',
+  reward: 5000,
+  captured: false }
 // 4. Query for a Groundhog in the Woodlands
+db.bounties.find({species: "Groundhog", location: "Woodlands"})
+{ _id: ObjectId("6311376909bab9eaf715c2a9"),
+  name: 'Grandhog',
+  species: 'Groundhog',
+  location: 'Woodlands',
+  wantedFor: 'Not coming out of the hole on time and prolonging winter',
+  client: 'Songbird',
+  reward: 50000,
+  captured: false }
 
 // Update and Delete
 // 1. Update the reward for Polarwind to 10000
+db.bounties.updateOne(
+  {name: "Polarwind"},
+  { $set: 
+  { reward: 10000}})
+{ acknowledged: true,
+  insertedId: null,
+  matchedCount: 1,
+  modifiedCount: 1,
+  upsertedCount: 0 }
+
+  db.bounties.find({name: "Polarwind"})
+  { _id: ObjectId("6311376909bab9eaf715c2a7"),
+    captured: false,
+    client: 'Sabertooth',
+    location: 'Arctic',
+    name: 'Polarwind',
+    reward: 10000,
+    species: 'Polar Bear',
+    wantedFor: 'Not hibernating' }
 
 // 2. Remove Lokinkajou
+db.bounties.deleteOne ({name: "Lokinkajou"})
+{ acknowledged: true, deletedCount: 1 }
 
 // 3. Delete all bounties sent by Songbird
+db.bounties.deleteMany ({client: "Songbird"})
+{ acknowledged: true, deletedCount: 3 }
+
 
 // 4. Update all captured statuses to true
+
+db.bounties.updateMany (
+    { captured: false},
+     { $set: {captured: true}
+     } )
+  { acknowledged: true,
+    insertedId: null,
+    matchedCount: 3,
+    modifiedCount: 3,
+    upsertedCount: 0 }
